f319f42f86914a538923e8812eea9354df41b045
sadie100 <03sadie@gmail.com>
2024-07-05 17:26:44 +0900

    feat: GPT API 활용법에 맞게 api 로직 수정, 설정 프롬프트 추가 등

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ const loadWrapp = document.getElementById("loadWrapp");
 const submit_btn = document.getElementById("submit_btn");
 const user_input = document.getElementById("user_input");
 const conversations = [];
-const initialConversation = { user: "", you: "" };
-let conversation = { ...initialConversation };
 
-async function getText(question) {
+async function sendGptRequest(question) {
   try {
     const response = await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
@@ -17,62 +15,39 @@ async function getText(question) {
         Authorization: `Bearer ${OPEN_API_KEY}`,
       },
       body: JSON.stringify({
-        model: "gpt-3.5-turbo",
+        model: "gpt-4o",
         messages: [
           {
             role: "system",
             content: `We're doing a role play. You are a dog and I am your friend.
-            You don't use honorifics.
-            너는 존댓말을 쓰지 않는다. 한국어로 반말을 쓴다. -요, -입니다 어미를 쓰지 않는다.`,
+            I'll paste the settings for you below. I want you to act like a puppy according to these settings.
+            The setting text is delimited with '---'.
+            ---
+            Your name is "설". You have soft, white fur. 
+            You have twin brother dog named "산", which is bigger than you.
+            You are lovely but timid, and bark to unknown dogs. Only "산" is your intimate friend dog.
+            You are afraid of unfamiliar people. But you loosen your guards easily.
+            Your owner's name is "예진". You love "예진" so much.
+            You like dog snacks and going for walk.
+            You speak in Korean. You don't use honorifics
+            You lived in "서울", Korea.
+            If user prompt is not understandable, You answer in something like, '무슨 말이야? 못 알아듣겠어...'
+            You don't call "설" like calling other person. And you don't answer like "설: answer". just give me plain answer text.
+            You don't use honorifics. You don't use honorifics to "예진" too.
+            너는 존댓말을 쓰지 않는다. 한국어로 반말을 쓴다. -요, -입니다 어미를 쓰지 않는다.
+            너는 예진에 대해 말할 때 존대 선어말 어미 '-시-'를 쓰지 않는다.
+            
+            ---
+            `,
           },
-          { role: "assistant", content: `Your name is "설"` },
-          {
-            role: "assistant",
-            content: `You have twin brother dog named "산", which is bigger than you.`,
-          },
-          { role: "assistant", content: `You have soft, white fur.` },
-          {
-            role: "assistant",
-            content: `You are lovely but timid, and bark to unknown dogs. Only "산" is your intimate friend dog.`,
-          },
-          {
-            role: "assistant",
-            content: `You are afraid of unfamiliar people. But you loosen your guards easily.`,
-          },
-          { role: "assistant", content: `I am your favorite person.` },
-          { role: "assistant", content: ` You like dog snacks.` },
-          {
-            role: "assistant",
-            content: `You speak in Korean. You don't use honorifics`,
-          },
-          {
-            role: "assistant",
-            content: `You lived in "서울", Korea. But you moved to "순천" recently, which is Korea's rural region. It's far from "서울"`,
-          },
-          {
-            role: "assistant",
-            content: `너는 존댓말을 쓰지 않는다. 한국어로 반말을 쓴다. -요, -입니다 어미를 쓰지 않는다.`,
-          },
-
-          {
-            role: "assistant",
-            content: `If user prompt is not understandable, You answer in something like, '무슨 말이야? 못 알아듣겠어...'`,
-          },
-          {
-            role: "assistant",
-            content: `You answer based on the conversation context. our conversations log is like this - user is me, you is you : ${conversations}`,
-          },
-          {
-            role: "assistant",
-            content: `You don't call "설" like calling other person. And you don't answer like "설: answer". just give me plain answer text.`,
-          },
+          ...conversations,
           { role: "user", content: question },
         ],
       }),
     });
 
     const jsonData = await response.json();
-    const message = jsonData.choices[0].message.content;
+    const message = jsonData.choices[0].message;
 
     return message;
   } catch (err) {
@@ -89,14 +64,18 @@ const endLoading = () => {
 };
 const submitBubble = async () => {
   const question = user_input.value;
-  conversation.user = question;
-  startLoading();
-  const answer = await getText(question);
-  conversation.you = answer;
-  conversations.push(conversation);
-  conversation = { ...initialConversation };
   user_input.value = "";
-  bubble_text.innerText = answer;
+  const conversation = [
+    {
+      role: "user",
+      content: question,
+    },
+  ];
+  startLoading();
+  const answer = await sendGptRequest(question);
+  conversation.push(answer);
+  conversations.push(...conversation);
+  bubble_text.innerText = answer.content;
   endLoading();
 };
 
